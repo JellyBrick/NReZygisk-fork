42076e5534478854e3fac6c13c8e35e86424380b
nampud <nampud@users.noreply.github.com>
2025-07-01 01:56:30 +0200

    Set a basic set of props

--- module/src/service.sh
+++ module/src/service.sh
@@ -21,3 +21,47 @@ if [ "$(which magisk)" ]; then
     fi
   done
 fi
+
+p_s() {
+  curr="$(resetprop "$1")"
+  if [ -n "$curr" ] && [ "$curr" != "$2" ]; then
+    resetprop -n "$1" "$2"
+  fi
+}
+
+p_r() {
+  case "$(resetprop "$1")" in
+    *"$2"*) resetprop -n "$1" "$3" ;;
+  esac
+}
+
+resetprop -w sys.boot_completed 0
+
+p_s ro.build.type user
+p_s ro.build.tags release-keys
+p_s ro.boot.vbmeta.device_state locked
+p_s ro.boot.verifiedbootstate green
+p_s vendor.boot.vbmeta.device_state locked
+p_s vendor.boot.verifiedbootstate green
+p_s sys.oem_unlock_allowed 0
+p_s ro.boot.veritymode enforcing
+p_s ro.boot.veritymode.managed yes
+p_s ro.boot.flash.locked 1
+p_s ro.boot.warranty_bit 0
+p_s ro.warranty_bit 0
+p_s ro.vendor.boot.warranty_bit 0
+p_s ro.vendor.warranty_bit 0
+p_s ro.debuggable 0
+p_s ro.force.debuggable 0
+p_s ro.secure 1
+p_s ro.adb.secure 1
+p_s ro.boot.realmebootstate green
+p_s ro.boot.realme.lockstate 1
+p_s ro.secureboot.lockstate locked
+p_s ro.is_ever_orange 0
+
+p_r ro.bootmode recovery unknown
+p_r ro.boot.mode recovery unknown
+p_r ro.boot.bootmode recovery unknown
+p_r vendor.boot.mode recovery unknown
+p_r vendor.boot.bootmode recovery unknown
