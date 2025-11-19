8ca7579f91e0bf072cda92f793bc1685e7102312
nampud <nampud@users.noreply.github.com>
2025-11-19 23:31:46 +0100

    hopefully fix CI

--- zygiskd/build.gradle.kts
+++ zygiskd/build.gradle.kts
@@ -46,7 +46,7 @@ else
 val CStandardFlags = baseCStandardFlags + zkVersionFlag
 
 val CFlagsRelease = arrayOf(
-  "-Wl,--strip-all", "-flto=thin", "-Ofast"
+  "-Wl,--strip-all", "-flto=thin", "-O3"
 )
 
 val CFlagsDebug = arrayOf(
